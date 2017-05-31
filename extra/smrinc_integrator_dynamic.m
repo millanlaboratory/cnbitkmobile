f295c0b9c947eb6cc7c2be33b6600791738a2774
Luca Tonin <luca.tonin@gmail.com>
2017-05-31 19:42:13 +0200

    new bci force profile. updated xml

--- extra/smrinc_integrator_dynamic.m
+++ extra/smrinc_integrator_dynamic.m
@@ -3,16 +3,21 @@ function y = smrinc_integrator_dynamic(x, yp, p, phi, dt)
     cx = 0:0.01:1;
     mf = max(abs(polyval(p, cx)));
 
-%     plot(cx, polyval(p, cx)./mf);
-%     keyboard
-
 
     Ff = phi*polyval(p, yp)./mf;
-%     Fb = beta*(x-0.5)*exp(-(x-0.5).^2);
-%     Fb = beta*(0.5-x)*exp(-(0.5-x).^2);
-%      Fb = beta*(0.5-x);
+
+    % New bci force
+    %Fb = (1-phi)*sin(pi*(x-yp)/4);
     
-    Fb = (1-phi)*polyval(p, x)./mf;
+    % New new bci force
+    x1 = [-0.5 -0.25 0 0.25 0.5];
+    y1 = [-1 -0.2 0 0.2 1];
+    %coeff = polyfit(x1, y1, 4);
+    coeff = [-0.0000    6.4000    0.0000    0.4000    0.0000];  % to speed-up
+    Fb = (1-phi)*polyval(coeff, x-0.5);
+    
+    % Original bci force
+    % Fb = (1-phi)*polyval(p, x)./mf;
     y = yp + dt*(Ff + Fb);
 
 end
