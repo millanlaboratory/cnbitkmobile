5993bb844717f2ca5c513e4946a3900373be0a8f
Luca Tonin <luca.tonin@gmail.com>
2017-08-14 11:40:59 +0200

    ndf matlab for mobile and launcher

--- extra/ndf/ndf_smr_mobile.m
+++ extra/ndf/ndf_smr_mobile.m
@@ -11,7 +11,7 @@ try
     
 	% Connect to the CnbiTk loop
 	if(cl_connect(loop.cl) == false)
-		disp('[ndf_smrinc] Cannot connect to CNBI Loop, killing matlab');
+		disp('[ndf_smr_mobile] Cannot connect to CNBI Loop, killing matlab');
 		exit;
 	end	
 
@@ -24,7 +24,7 @@ try
 	%   their values will be set according to the XML configuration
 	loop = ndf_loopconfig(loop, 'mi');
 	if(loop.cfg.config == 0)
-		disp('[ndf_smrinc] Cannot retrieve loop configuration, killing matlab');
+		disp('[ndf_smr_mobile] Cannot retrieve loop configuration, killing matlab');
 		exit;
 	end
 	loop = ndf_loopnames(loop);
@@ -45,7 +45,7 @@ try
      
 	
 	if(isempty(loop.cfg.ndf.pipe))
-		disp('[ndf_smrinc] NDF configuration failed, killing matlab:');
+		disp('[ndf_smr_mobile] NDF configuration failed, killing matlab:');
 		disp(['  Pipename:   "' loop.cfg.ndf.pipe '"']);
 		disp(['  iC address: "' loop.cfg.ndf.ic '"']);
 		disp(['  iD address: "' loop.cfg.ndf.id '"']);
@@ -68,7 +68,7 @@ try
 	% Pipe opening and NDF configurationf
 	% - Here the pipe is opened
 	% - ... and the NDF ACK frame is received
-	disp('[ndf_smrinc] Receiving ACK...');
+	disp('[ndf_smr_mobile] Receiving ACK...');
 	[ndf.conf, ndf.size] = ndf_ack(ndf.sink);
     
     % Read in parameters
@@ -86,8 +86,8 @@ try
     integrator.nprobs    = 0.5;
     integrator.type = cl_retrieveconfig(loop.cl, 'integrator', 'type');
     if isempty(integrator.type)
-        disp('[ndf_smrinc] Cannot retrieve the integrator type from nameserver')
-        disp('[ndf_smrinc] Killing MATLAB')
+        disp('[ndf_smr_mobile] Cannot retrieve the integrator type from nameserver')
+        disp('[ndf_smr_mobile] Killing MATLAB')
         exit;
     end
     integrator.filepath = ['/tmp/cnbitk-' getenv('USER') '/' datestr(now,'yyyymmdd') '.smrinc_parameters.txt'];
@@ -117,16 +117,35 @@ try
         end  
     catch exception
         ndf_printexception(exception);
-        disp('[ndf_smrinc] Killing Matlab...');
+        disp('[ndf_smr_mobile] Killing Matlab...');
         exit;
     end
     
-	%msg_thresholds_log = 'thresholds=(';
+    % Retrieve control type and update log file
+    control.continuous = false;
+    control.discrete   = false;
+    control.type = cl_retrieveconfig(loop.cl, 'control', 'type');
+    if isempty(control.type)
+        disp('[ndf_smr_mobile] Cannot retrieve the control type from nameserver')
+        disp('[ndf_smr_mobile] Killing MATLAB')
+        exit;
+    end
+    switch(control.type)
+        case 'continuous'
+            control.continuous = true;
+        case 'discrete'
+            control.discrete = true;
+        otherwise
+            disp(['[ndf_smr_mobile] - Unknown control type: ' control.type '. Exit']);
+            exit;
+    end
+    disp(['[ndf_smr_mobile] - Control type set as ' control.type])
+    cl_updatelog(loop.cl, sprintf('control=%s', control.type));
+    
+    % Updating log file with thresholds
     for t = 0:user.nTasks-1
         cl_updatelog(loop.cl, ['th_' num2str(user.tasklabel{t+1}) '=' num2str(user.thresholds(t+1))]);
-	    %msg_thresholds_log = sprintf('%s%f', msg_thresholds_log, user.thresholds(t+1));
     end
-	%msg_thresholds_log = sprintf('%s)', msg_thresholds_log);
 	
 
 	% -------------------------------------------------------------- %
@@ -141,19 +160,19 @@ try
         user.bci.support.rawprobs = 0.5;                                        % Added field
     catch exception
 		ndf_printexception(exception);
-        disp('[ndf_smrinc] Killing Matlab...');
+        disp('[ndf_smr_mobile] Killing Matlab...');
         exit;
     end
 
 	if(ndf.conf.eeg_channels == 16)
-		disp('[ndf_smrinc] Running with single gUSBamp');
+		disp('[ndf_smr_mobile] Running with single gUSBamp');
     else
-		disp('[ndf_smrinc] Running with two synced gUSBamps');        
+		disp('[ndf_smr_mobile] Running with two synced gUSBamps');        
 	end
 	
 	if(user.bci.analysis.settings.eeg.fs ~= ndf.conf.sf)
-		disp('[ndf_smrinc] Error: NDF sample rate differs from classifier settings');
-        disp('[ndf_smrinc] Killing Matlab...');
+		disp('[ndf_smr_mobile] Error: NDF sample rate differs from classifier settings');
+        disp('[ndf_smr_mobile] Killing Matlab...');
         exit;
 	end
 
@@ -165,13 +184,13 @@ try
 	% NDF ACK check
 	% - The NDF id describes the acquisition module running
 	% - Bind your modules to a particular configuration (if needed)
-	disp(['[ndf_smrinc] NDF type id: ' num2str(ndf.conf.id)]);
+	disp(['[ndf_smr_mobile] NDF type id: ' num2str(ndf.conf.id)]);
     
 	% Initialize ndf_jump structure
 	% - Each NDF frame carries an index number
 	% - ndf_jump*.m are methods to verify whether your script is
 	%   running too slow
-	disp('[ndf_smrinc] Receiving NDF frames...');
+	disp('[ndf_smr_mobile] Receiving NDF frames...');
 	loop.jump.tic = ndf_tic();
     
     
@@ -185,7 +204,7 @@ try
 
 		% Acquisition is down, exit
 		if(ndf.size == 0)
-			disp('[ndf_smrinc] Broken pipe');
+			disp('[ndf_smr_mobile] Broken pipe');
 			break;
 		end
 		
@@ -205,7 +224,7 @@ try
                 if(isempty(useralpha) == false)
                     if(str2double(useralpha) ~= double(integrator.param.alpha))
                         integrator.param.alpha = str2double(useralpha);
-                        disp(['[ndf_smrinc] - DEBUG: Changed alpha parameter for ema to: ' useralpha]);
+                        disp(['[ndf_smr_mobile] - DEBUG: Changed alpha parameter for ema to: ' useralpha]);
                     end
                 end
                 integrator.nprobs = smrinc_integrator_ema(integrator.cprobs, integrator.nprobs, integrator.param.alpha, integrator.param.rejection);
@@ -214,14 +233,14 @@ try
                 if(isempty(userphi) == false)
                     if(str2double(userphi) ~= double(integrator.param.phi))
                         integrator.param.phi = str2double(userphi);
-                        disp(['[ndf_smrinc] - DEBUG: Changed phi parameter for dynamic to: ' userphi]);
+                        disp(['[ndf_smr_mobile] - DEBUG: Changed phi parameter for dynamic to: ' userphi]);
                     end
                 end
                 userchi = ndf_read_param(integrator.filepath, 'integrator', 'dynamic', 'chi');
                 if(isempty(userchi) == false)
                     if(str2double(userchi) ~= double(integrator.param.chi))
                         integrator.param.chi = str2double(userchi);
-                        disp(['[ndf_smrinc] - DEBUG: Changed chi parameter for dynamic to: ' userchi]);
+                        disp(['[ndf_smr_mobile] - DEBUG: Changed chi parameter for dynamic to: ' userchi]);
                     end
                 end
                 integrator.nprobs = smrinc_integrator_dynamic(integrator.cprobs, integrator.nprobs, integrator.coeff, integrator.param.phi, integrator.param.chi, integrator.dt);
@@ -231,13 +250,13 @@ try
                 if(isempty(userrho) == false)
                     if(str2double(userrho) ~= double(integrator.param.rho))
                         integrator.param.rho = str2double(userrho);
-                        disp(['[ndf_smrinc] - DEBUG: Changed rho parameter for vema to: ' userrho]);
+                        disp(['[ndf_smr_mobile] - DEBUG: Changed rho parameter for vema to: ' userrho]);
                     end
                 end
                 if(isempty(usergamma) == false)
                     if(str2double(usergamma) ~= double(integrator.param.gamma))
                         integrator.param.gamma = str2double(usergamma);
-                        disp(['[ndf_smrinc] - DEBUG: Changed gamma parameter for vema to: ' usergamma]);
+                        disp(['[ndf_smr_mobile] - DEBUG: Changed gamma parameter for vema to: ' usergamma]);
                     end
                 end
                 
@@ -251,7 +270,7 @@ try
                 
                 if(id_event == 781)
                     integrator.nprobs = 0.5;
-					printf('[ndf_smrinc] Resetting NDF=%d/iD=%d\n', ndf.frame.index, idmessage_getbidx(loop.mDi));
+					printf('[ndf_smr_mobile] Resetting NDF=%d/iD=%d\n', ndf.frame.index, idmessage_getbidx(loop.mDi));
                 end
             end
         else
@@ -264,8 +283,8 @@ try
                 % Map the tasklabel to the probabilities output from EEGC3
                 taskloc = find(user.tasklabel{t} == user.bci.analysis.settings.task.classes_old);
                 if length(taskloc) ~= 1
-                    disp('[ndf_smrinc] Task not present in classifier')
-                    disp('[ndf_smrinc] Killing MATLAB')
+                    disp('[ndf_smr_mobile] Task not present in classifier')
+                    disp('[ndf_smr_mobile] Killing MATLAB')
                     exit;
                 end
                 integrator.sprob = [integrator.nprobs 1-integrator.nprobs];
@@ -277,11 +296,13 @@ try
         end
         
         % Handle sync TOBI iC communication to mobile
-        if(tic_isattached(loop.mobile.iC) == true)
-			icmessage_setvalue(loop.mobile.mC, loop.cfg.classifier.id, num2str(loop.cfg.ndf.mobile.label), integrator.nprobs);
-			tic_setmessage(loop.mobile.iC, loop.mobile.sC, ndf.frame.index);
-		else
-			tic_attach(loop.mobile.iC, loop.cfg.ndf.mobile.ic);
+        if(control.continuous == true)
+            if(tic_isattached(loop.mobile.iC) == true)
+                icmessage_setvalue(loop.mobile.mC, loop.cfg.classifier.id, num2str(loop.cfg.ndf.mobile.label), integrator.nprobs);
+                tic_setmessage(loop.mobile.iC, loop.mobile.sC, ndf.frame.index);
+            else
+                tic_attach(loop.mobile.iC, loop.cfg.ndf.mobile.ic);
+            end
         end
         
 		% -------------------------------------------------------------- %
@@ -291,13 +312,13 @@ try
 		% Check if module is running slow
 		loop.jump = ndf_jump_update(loop.jump, ndf.frame.index);
         if(loop.jump.isjumping)
-			disp(['[ndf_smrinc] Error: running slow ' num2str(ndf_toc(loop.uptime)/1000/60)]);
+			disp(['[ndf_smr_mobile] Error: running slow ' num2str(ndf_toc(loop.uptime)/1000/60)]);
         end
         
 	end
 catch exception
 	ndf_printexception(exception);
-	disp('[ndf_smrinc] Going down');
+	disp('[ndf_smr_mobile] Going down');
 	loop.exit = true;
 end
 
@@ -309,11 +330,11 @@ catch exception
 	loop.exit = true;
 end
 
-disp(['[ndf_smrinc] Loop uptime: ' ...
+disp(['[ndf_smr_mobile] Loop uptime: ' ...
 	num2str(ndf_toc(loop.uptime)/1000/60) ...
 	' minutes']);
 
-disp('[ndf_smrinc] Killing Matlab...');
+disp('[ndf_smr_mobile] Killing Matlab...');
 if(loop.exit == true)
 	exit;
 end
